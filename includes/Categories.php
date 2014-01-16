8b822c37ae27d4858a7e275cc7e3df4e80be20f2
Alfred <alfredayibonte@yahoo.com>
2014-01-16 11:17:32 +0000

    created edit school and delete school in school.php

--- includes/Categories.php
+++ includes/Categories.php
@@ -1,14 +1,35 @@
 <?php
+require_once("Database.php");
 class Categories
 {
-	function Categories()
+	private $id;
+	private $inclusive;
+	private $teachlearn;
+	private $safepro;
+	private $healthy;
+	private $commeng;
+	private $gender;
+	function Categories($id, $inclusive, $teachlearn, $safepro, $healthy, $commeng, $gender)
 	{
-		private $health;
-		private $inclusive;
-		private $safety;
-		private $gender;
-		private $communitengagement;
-		
+		$this->id = $id;
+		$this->inclusive = $inclusive;
+		$this->teachlearn = $teachlearn;
+		$this->safepro = $safepro;
+		$this->healthy = $healthy;
+		$this->commeng = $commeng;
+		$this->gender = $gender;
+
 	}
+	public function storeCategory()
+	{
+		$database = new Database('localhost', 'root', '');
+		$database->connectdb();
+		$database->select('skuulcheck');
+		$sql = "INSERT INTO `skuulcheck`.`categories` (`inclusive`, `teachlearn`, `safepro`, `healthy`, `commeng`, `gender`) VALUES ";
+		$sql .= "('$this->inclusive', '$this->teachlearn', '$this->safepro', '$this->healthy', '$this->commeng', '$this->gender') ";
+		mysql_query($sql);
+		$database->disconnectdb();
+	}
+	
 }
 ?>
--- includes/School.php
+++ includes/School.php
@@ -4,25 +4,42 @@ class School
 {
 	private $schoolname;
 	private $schoolstreetname;
+	private $database;
 	
-	
-	function School($schoolname, $schoolstreetname)
+	function School()
+	{
+
+		$this->database = new Database('localhost', 'root', '');
+		$this->database->connectdb();
+		$this->database->select('skuulcheck');
+		
+	}
+	function storeSchool($schoolname, $schoolstreetname)
 	{
 		$this->schoolname = $schoolname;
 		$this->schoolstreetname = $schoolstreetname;
-	}
-	function storeSchool()
-	{
-		$database = new Database('localhost', 'root', '');
-		$database->connectdb();
-		$database->select('skuulcheck');
 		$sql = "INSERT INTO `skuulcheck`.`school` (`schoolname`, `schoolstreetname`) VALUES ";
 		$sql .= "('$this->schoolname', '$this->schoolstreetname') ";
 		mysql_query($sql);
-		$database->disconnectdb();
+		$this->database->disconnectdb();
+	}
+	function removeSchool($schoolname)
+	{
+		$sql = "DELETE FROM `skuulcheck`.`school` WHERE ";
+		$sql .= "`schoolname`='$schoolname'";
+		mysql_query($sql);
+		$this->database->disconnectdb();
+	}
+	function editSchool($formerschoolname, $currentschoolname)
+	{
+		$sql = "UPDATE `skuulcheck`.`school` SET `schoolname`='$currentschoolname' WHERE ";
+		$sql .= "`schoolname`='$formerschoolname'";
+		mysql_query($sql);
+		$this->database->disconnectdb();
 	}
 	
 }
-$school = new School("Hadi","10245032");
-$school->storeSchool();
+$school = new School();
+$school->storeSchool("Hadi International school","shockor");
+//$school->removeSchool("University of Ghana");
 ?>
